00bb18adec473a89f216ae198919eadc20b8f2c7
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-09-27 15:11:29 +0530

    added modified customer.java file

--- Class_definations/OUT_system/Customer.java
+++ Class_definations/OUT_system/Customer.java
@@ -1,10 +1,42 @@
 package OUT_system;
 
-public class Customer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Customer {
+	
+	
+	// class attributes 
+	
+	int Customer_ID;
+	String Customer_Name;
+	String Customer_Address;
+	int phone_No;
+	
+	// class function 
+	
+	
+	public Customer() {
+		Customer_ID = 0;
+	}
+    
+	
+	public void AddCustomer(int ID, String Name, String Address, int PhoneNO) {
+		
+		 // Database Add operation
+ 
+	}
+	
+	public void EditCustomer(int ID, String Name, String Address, int PhoneNO) {
+		
+		 // database edit operation
+ 
+	}
+	
+	public void DeleteCustomer(int ID) {
+		
+		 // database delete operation
 
 	}
+	
+	
 
 }
