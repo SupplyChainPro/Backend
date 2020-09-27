3b22c2aec9e817d351180dfb0f8a9f763af75760
sangram2200 <57897629+sangram2200@users.noreply.github.com>
2020-09-27 15:23:01 +0530

    added changes to payment.java
    added skeleton program of payment

--- Class_definations/IN_system/Payment.java
+++ Class_definations/IN_system/Payment.java
@@ -3,8 +3,30 @@ package IN_system;
 public class Payment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+}
 
+class payment{
+	int Paymant_Id;
+	String Payment_Amount;
+	int Paymant_Customer_Id;
+	date Paymant_Date;
+	
+	payment(){
+		 Paymant_Id=0;
+		 Payment_Amount="";
+		 Paymant_Customer_Id=0;
+		 Paymant_Date=00/00/0000;
+	
+	public void Add_Payment(int pid , String pamt , int pcid , pdate){
+		
 	}
-
+	
+	public void Edit_Payment(int pid , int pcid){
+		
+	}
+	
+	public void Search_Payment(int pid){
+		
+	}
 }
