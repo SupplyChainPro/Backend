d06287409ba28f97bde4a44d1312c95df2a0de1a
sangram2200 <57897629+sangram2200@users.noreply.github.com>
2020-09-27 15:14:08 +0530

    added changes to stock.java
    added the skeleton code

--- Class_definations/IN_system/Stock.java
+++ Class_definations/IN_system/Stock.java
@@ -3,8 +3,35 @@ package IN_system;
 public class Stock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+}
 
+class stock{
+	int Product_Id;
+	int Quantity;
+	int Supplier_Id;
+	
+	create_stock(){
+		Product_Id = 0;
+		Quantity = 0;
+		Supplier_Id = 0;
 	}
-
+	
+	create_stock(int pid , int quan , int sid){
+		Product_Id = pid;
+		Quantity = quan;
+		Supplier_Id = sid;
+	}
+	
+	public void Add_Stock(int pid , int quan , int sid){
+		
+	}
+	
+	public void Modify_Stock(int pid){
+		
+	}
+	
+	public void Display_Stock(int pid){
+		
+	}
 }
