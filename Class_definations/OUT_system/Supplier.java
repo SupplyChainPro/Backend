23d2e50877b831912fba803803681e03898dcb72
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-09-28 12:10:31 +0530

    added modified Supplier.java

--- Class_definations/OUT_system/Supplier.java
+++ Class_definations/OUT_system/Supplier.java
@@ -1,10 +1,42 @@
 package OUT_system;
 
-public class Supplier {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Supplier {
+	
+	
+	// class attributes 
+	
+	int Supplier_ID;
+	String Supplier_Name;
+	String Supplier_Address;
+	int phone_No;
+	
+	// class function 
+	
+	
+	public Supplier() {
+		Supplier_ID = 0;
+	}
+    
+	
+	public void AddSupplier(int ID, String Name, String Address, int PhoneNO) {
+		
+		 // Database Add operation
+ 
+	}
+	
+	public void EditSupplier(int ID, String Name, String Address, int PhoneNO) {
+		
+		 // database edit operation
+ 
+	}
+	
+	public void DeleteSupplier(int ID) {
+		
+		 // database delete operation
 
 	}
+	
+	
 
 }
