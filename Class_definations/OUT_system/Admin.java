b1fc4dfdd858a2a5a9d234b7ce9172d07ac1f23a
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-09-28 12:05:51 +0530

    added modified admin.java
    This file is developed by @onkar1010  I have just added it to the GitHub

--- Class_definations/OUT_system/Admin.java
+++ Class_definations/OUT_system/Admin.java
@@ -1,10 +1,32 @@
 package OUT_system;
 
-public class Admin {
+Class admin{
+    Int login_Id;
+    Int Admin_name;
+	
+	
+ 
+    Public void login(int id , int name )
+    {
+	// Basic login operation
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
+   Public void logout(int id , int name)
+   { 
+	//Basic logout operation
+   }
+	
+   Public void manage_order()
+   { 
+	//database manageorder operation
+   }
+	
+  Public void verify_supplier()
+  {
+       //database verify supplier operation
+  }
+	
+	
+	
 }
