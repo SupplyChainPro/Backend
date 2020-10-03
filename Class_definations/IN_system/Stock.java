66ea29669c75821a7a641828d862adddc2b53ad4
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-03 21:02:40 +0530

    UPDATE WORKING VERSION 0.0

--- Class_definations/IN_system/Stock.java
+++ Class_definations/IN_system/Stock.java
@@ -1,37 +1,26 @@
 package IN_system;
 
-public class Stock {
-
-	public static void main(String[] args) {
+public class Stock{
 	
-}
-
-class stock{
 	int Product_Id;
 	int Quantity;
 	int Supplier_Id;
 	
-	stock(){
+	Stock(){
 		Product_Id = 0;
 		Quantity = 0;
 		Supplier_Id = 0;
 	}
 	
-	stock(int pid , int quan , int sid){
+	public Stock(int pid , int quan , int sid){
 		Product_Id = pid;
 		Quantity = quan;
 		Supplier_Id = sid;
 	}
 	
-	public void Add_Stock(int pid , int quan , int sid){
+	public void showData(){
 		
-	}
-	
-	public void Modify_Stock(int pid){
-		
-	}
-	
-	public void Display_Stock(int pid){
+		System.out.println("\n"+Product_Id+" \t\t\t "+Quantity);
 		
 	}
 }
