089c3ec4df6b002c8c65c27acdca580ca6ff1978
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-03 21:03:21 +0530

    UPDATE WORKING VERSION 0.0

--- Class_definations/OUT_system/Admin.java
+++ Class_definations/OUT_system/Admin.java
@@ -1,32 +1,34 @@
 package OUT_system;
 
-Class admin{
-    Int login_Id;
-    Int Admin_name;
+class admin{
+	
+
+    int login_Id;
+    int Admin_name;
 	
 	
  
-    Public void login(int id , int name )
+    public void login(int id , int name )
     {
 	// Basic login operation
     }
 
 
-   Public void logout(int id , int name)
+   public void logout(int id , int name)
    { 
 	//Basic logout operation
    }
 	
-   Public void manage_order()
+   public void manage_order()
    { 
 	//database manageorder operation
+	 
    }
 	
-  Public void verify_supplier()
+  public void verify_supplier()
   {
        //database verify supplier operation
   }
-	
-	
-	
+  
 }
+	
