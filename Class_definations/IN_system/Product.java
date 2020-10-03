ad821a2733eda8dff812d2dc546458c462fda30a
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-03 21:01:55 +0530

    UPDATE WORKING VERSION 0.0

--- Class_definations/IN_system/Product.java
+++ Class_definations/IN_system/Product.java
@@ -1,45 +1,29 @@
 package IN_system;
 
 public class Product {
-    public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int Product_Id;
+	int Supplier_Id;
+	
+	public Product(){
+		Product_Id = 0;
+		Supplier_Id = 0;
 	}
+	
+	public Product(int pid  , int sid){
+		Product_Id = pid;
+		Supplier_Id = sid;
+	}
+	
+	public void showData(){																													
+		
+		System.out.println("\n"+Product_Id+" \t\t\t "+Supplier_Id);
+		
+	}
+	public void Delete_Product(int pid){
+		
+	}
+	
+	
+	
 }
 
-class product{
-    int product_ID;
-    String product_name;
-    float price;
-    int quantity;
-
-    product(){
-        product_ID=0;
-        product_name="";
-        price=0;
-        quantity=0;
-    }
-
-    product(int p_ID; String p_name; float p; int q){
-        product_ID=p_ID;
-        product_name=p_name;
-        price=p;
-        quantity=q;
-    }
-
-    public void Add_product(){
-        //get product details and add to the database.
-    }
-
-    public void Search_product(){
-        //search given product
-    }
-
-    public void Modify_product(){
-        //get product from database to edit.update database with modified data
-    }
-
-    public void Delete_product(){
-        //delete given product
-    }
-}
