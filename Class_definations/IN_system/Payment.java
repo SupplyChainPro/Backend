dc72f73934b8099048947ebfc7e8265e29741676
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-03 21:00:46 +0530

    UPDATE WORKING VERSION 0.0

--- Class_definations/IN_system/Payment.java
+++ Class_definations/IN_system/Payment.java
@@ -1,32 +1,31 @@
 package IN_system;
 
-public class Payment {
 
-	public static void main(String[] args) {
-	
-}
-
-class payment{
+public class Payment{
 	int Paymant_Id;
-	String Payment_Amount;
+	int Payment_Amount;
 	int Paymant_Customer_Id;
-	date Paymant_Date;
 	
-	payment(){
+	
+	public Payment(){
 		 Paymant_Id=0;
-		 Payment_Amount="";
+		 Payment_Amount=0;
 		 Paymant_Customer_Id=0;
-		 Paymant_Date=00/00/0000;
+		}
 	
-	public void Add_Payment(int pid , String pamt , int pcid , pdate){
-		
+	public Payment(int pid , int pamt , int pcid ){
+		 Paymant_Id=pid;
+		 Payment_Amount=pamt;
+		 Paymant_Customer_Id=pcid;
 	}
 	
-	public void Edit_Payment(int pid , int pcid){
-		
+	public void Display(){
+		System.out.println("\n"+Paymant_Id+" \t\t\t "+Payment_Amount+" \t\t\t"+Paymant_Customer_Id);
 	}
 	
-	public void Search_Payment(int pid){
+	public void Search_Payment(int pid , int m[]){
+		
 		
 	}
+
 }
