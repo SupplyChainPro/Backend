f98cb71c17d58c5540100cc99ccc01b53685a777
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-31 17:25:53 +0530

    Updated

--- Class_definations/IN_system/Order.java
+++ Class_definations/IN_system/Order.java
@@ -1,6 +1,6 @@
 package IN_system;
 
-
+// Import check_stock_avl from Stock  
 public class Order{
 	
     int Order_ID;
@@ -26,6 +26,14 @@ public class Order{
     }
 
     public void Add_order(int o_ID,int c_ID,String c_name, float a, String o_date){
+	    
+	    
+	// Stock Avlb  --- > ( check for the avlb Of that product ) { by calling Stock.java } 
+		// Connect to the Database via JDBC 
+	    	// Perform add data operation to Mysql DB server  
+	// else 
+	    	// Retrive back with Out of stock 
+	    
     	Order_ID=o_ID;
         Customer_ID=c_ID;
         Customer_name=c_name;
@@ -34,11 +42,24 @@ public class Order{
     	
     }
 
-    public void Edit_order(){
+    public void Edit_order(int o_ID,int c_ID,String c_name, float a, String o_date ){
         //get order from database to edit.update databse with modified data
+	    
+	    
+	// Check for the Order_ID
+		// Connect to the Database via JDBC 
+		// Perform edit data operation to Mysql DB server 
+	// else 
+	    	// Retrive back with Error msg " Invalid Order_ID "  
     }
 
     public void Cancel_order(int ID){
+	    
+	// Check for the Order_ID
+		// Connect to the Database via JDBC 
+		// Perform edit data operation to Mysql DB server 
+	// else 
+	    	// Retrive back with Error msg " Invalid Order_ID "  
     	
     	Order_ID=0;
         Customer_ID=0;
