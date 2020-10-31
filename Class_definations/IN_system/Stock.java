c15b4378e316df65ac1e2dedae1751a9afaf7f6a
Vaibhav R Mankar <59453531+mankar1257@users.noreply.github.com>
2020-10-31 17:39:16 +0530

    Update Stock.java

--- Class_definations/IN_system/Stock.java
+++ Class_definations/IN_system/Stock.java
@@ -1,4 +1,5 @@
 package IN_system;
+// import Product 
 
 public class Stock{
 	
@@ -16,9 +17,32 @@ public class Stock{
 		Product_Id = pid;
 		Quantity = quan;
 		Supplier_Id = sid;
+		
+		 
+	}
+	/* Check_Stock_Avl(Product_ID) -- > New function 
+	{ connect to the JDBC 
+		Check the Stock Count of the Product == Product_ID
+		if count == 0 : 
+			rturn -1
+		else :
+			return Stock_count 
 	}
 	
-	public void showData(){
+
+	Add_Stock(int pid , int quan , int sid){
+		 connect to the JDBC 
+			Create new Data for supplier 
+			}
+	Update_stock ( int pid , int quan , int sid ) {
+		   connect to the JDBC 
+		   	Upadate data
+			}
+			
+	
+	public void showData(Supplier_ID) {
+		 connect to the JDBC 
+			  Retrive all data from the database with Supplier_ID == Supplier_ID 
 		
 		System.out.println("\n"+Product_Id+" \t\t\t "+Quantity);
 		
