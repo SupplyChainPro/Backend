8fe488f936bef4c1fed7c12b7a1b9e114261aa44
mankar1257 <mankarvaibhav819@gmail.com>
2020-11-01 22:10:41 +0530

    just checking my git local setup

--- Class_definations/DB_Utilities/DB_Connect.java
+++ Class_definations/DB_Utilities/DB_Connect.java
@@ -7,40 +7,43 @@ public class DB_Connect {
 
 	public Connection con = null ;
 	public Statement statement = null;
-	public PreparedStatement preparedStatement = null; 
+	public PreparedStatement preparedStatement = null;
 	public ResultSet resultSet = null;
-	
+
     public Connection Connect(String Conn_URL , String Username , String Password) throws Exception {
-    	
-    	try{  
-    		Class.forName("com.mysql.jdbc.Driver");  
-    		
-    		con=DriverManager.getConnection(Conn_URL, Username,Password);  
-    		 
+    	/* function provides sequre way to connnect to the database
+			and return connection string */
+    	try{
+    		Class.forName("com.mysql.jdbc.Driver");
+
+    		con=DriverManager.getConnection(Conn_URL, Username,Password);
+
     		return con;
-    		
+
     		}
-    	
-    	catch(Exception e){ 
+
+    	catch(Exception e){
     		throw e;
-    
-    		}  
-    	
-    } 
-    
-    
+
+    		}
+
+    }
+
+
     public ResultSet Run_Query(String SQL_Query) throws SQLException {
-    	
-    	statement =con.createStatement();  
-    	
-    	resultSet =statement.executeQuery(SQL_Query);  
-    	
+			/* function provides a way to run the SQL Qureies directly on
+			the SQL database */
+    	statement =con.createStatement();
+
+    	resultSet =statement.executeQuery(SQL_Query);
+
     	return resultSet;
-		
+
 	}
-    
-    
+
+
     public void Close() throws Exception {
+			/* for closing all the connections */
     	 try {
              if (resultSet != null) {
                  resultSet.close();
@@ -54,14 +57,12 @@ public class DB_Connect {
                  con.close();
              }
     	 }
-    	catch(Exception e){ 
+    	catch(Exception e){
     		throw e;
     	}
     }
-    
-    
-  
-    
-}
-    
 
+
+
+
+}
