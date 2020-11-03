57e2aba06a3f563f83722bd5d933feec1fccdf9f
sangram2200 <57897629+sangram2200@users.noreply.github.com>
2020-11-03 09:52:33 +0530

    added wire frame for JDBC changes
    also added validate customer login fuction

--- Class_definations/OUT_system/Customer.java
+++ Class_definations/OUT_system/Customer.java
@@ -20,19 +20,46 @@ class Customer {
     
 	
 	public void AddCustomer(long ID, String Name, String Address, long PhoneNO) {
-		
+		//Conect to database via JDBC
+		//Check if customer already exists
+			//if yes
+				//retrieve back to login page with msg "Customer already exists"
+			//if no
+				//continue with below function
 		 
 		Customer_ID = ID;
 		Customer_Name = Name;
 		Customer_Address = Address;
 		phone_No = PhoneNO;
 		
+		//add info to database
  
 	}
+	/*
+	//To Validate customer login
+	public void validate(long ID , String password){
+		 //Conect to database via JDBC
+		 //check for valid info
+		 	if ID matched in database but password doesn't
+				//retrieve with message "Wrong password"
+			
+			if both ID and password matched in database
+				//login the customer in the system
+				
+			else 
+				//retrieve with msg "Customer doesn't exist"
+	}
+	*/
 	
 	public void EditCustomer(long ID, String Name, String Address, long PhoneNO) {
 		
-		 
+		 //Conect to database via JDBC
+		//Check if customer exists
+			//if yes
+				//let customer edit the info
+				//update in database
+			//if no
+				//retrieve with error msg "customer doesn't exist"
 		Customer_ID = ID;
 		Customer_Name = Name;
 		Customer_Address = Address;
@@ -41,6 +68,14 @@ class Customer {
 	}
 	 
 	public void DeleteCustomer(long ID) {
+		
+		//Conect to database via JDBC
+		//Check if customer exists
+			//if yes
+				//delete the customer from database
+			//if no
+				//retrieve with error msg "customer doesn't exist"
+		Customer_ID = ID;
 		
 		Customer_ID = 0;
 		Customer_Name = "";
