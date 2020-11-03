381ad7266c0a257e4ff24121f625df25a82935c5
sangram2200 <57897629+sangram2200@users.noreply.github.com>
2020-11-03 09:58:05 +0530

    wire frame for JDBC
    added validate supplier algo

--- Class_definations/OUT_system/Supplier.java
+++ Class_definations/OUT_system/Supplier.java
@@ -21,7 +21,13 @@ class Supplier {
 		
 		public void AddSupplier(long ID, String Name, String Address, long PhoneNO) {
 			
-			 
+			 //Conect to database via JDBC 
+			//Check if supplier already exists
+			//if yes
+				//retrieve back to login page with msg "Supplier already exists"
+			//if no
+				//continue with below function
+			
 			Supplier_ID = ID;
 			Supplier_Name = Name;
 			Supplier_Address = Address;
@@ -29,10 +35,27 @@ class Supplier {
 			
 	 
 		}
-		
+		/*
+		//To Validate supplier login
+		public void validate(long ID , String password){
+			 //Conect to database via JDBC
+			 //check for valid info
+				if ID matched in database but password doesn't
+					//retrieve with message "Wrong password"
+
+				if both ID and password matched in database
+					//login the supplier in the system
+
+				else 
+					//retrieve with msg "Supplier doesn't exist"
+		}
+		*/
 		public void EditSupplier(long ID, String Name, String Address, long PhoneNO) {
 			
-			 
+			  //Conect to database via JDBC
+				//let supplier edit the info
+				//update in database
+			
 			Supplier_ID = ID;
 			Supplier_Name = Name;
 			Supplier_Address = Address;
@@ -41,6 +64,10 @@ class Supplier {
 		}
 		
 		public void DeleteSupplier(long ID) {
+			
+			//Conect to database via JDBC
+				//Delete the supplier from database
+				//update in database
 			
 			Supplier_ID = 0;
 			Supplier_Name = "";
